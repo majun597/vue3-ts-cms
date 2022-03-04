0040dd5c879f5a2d655a33509d1c4790ada0b9b7
MaJun18705161998 <2303005592@qq.com>
2022-03-04 12:27:07 +0800

    test01

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,8 +12,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 });
 </script>
 
